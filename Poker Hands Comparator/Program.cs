c2734fc10ec291e899fe82b2df3630c9bea9fdc4
Jacob Walker <jacob.walker94@gmail.com>
2013-04-05 00:40:52 +0200

    Modified Main to complete comparison process
    Main (Program.cs) now does the comparison and outputs
    an appropriate message.

--- Poker Hands Comparator/Program.cs
+++ Poker Hands Comparator/Program.cs
@@ -15,7 +15,22 @@ namespace Poker_Hands_Comparator
             Hand firstHand = dealer.GetValidHandOrDieTrying();
             Hand secondHand = dealer.GetValidHandOrDieTrying();
 
-            firstHand.CompareTo(secondHand);
+            Hand.ComparisonOutcome outcome = firstHand.CompareTo(secondHand);
+
+            if (outcome == Hand.ComparisonOutcome.Win)
+            {
+                Console.WriteLine("The first hand wins.");
+            }
+            else if (outcome == Hand.ComparisonOutcome.Lose)
+            {
+                Console.WriteLine("The second hand wins.");
+            }
+            else // Outcome = Hand.ComparisonOutcome.Draw
+            {
+                Console.WriteLine("The hands are tied.");
+            }
+            
+            Console.ReadKey();
         }
     }
 }
