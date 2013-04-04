6689c2f5a1648c80005be252863673c8d111bd58
Jacob Walker <jacob.walker94@gmail.com>
2013-04-04 14:13:33 +0100

    Created Card class

---
+++ Poker Hands Comparator/Card.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poker_Hands_Comparator
+{
+    class Card
+    {
+        private char suit;
+        private char value;
+
+        public Card(char _suit, char _value)
+        {
+            this.suit = _suit;
+            this.value = _value;
+        }
+    }
+}
