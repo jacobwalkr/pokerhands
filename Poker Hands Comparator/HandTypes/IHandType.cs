34663d488b0a921ecd0aec3eeef87f9262a29ee9
Jacob Walker <jacob.walker94+github@gmail.com>
2013-04-06 15:04:58 +0100

    Added hand type interface, IHandType.cs

---
+++ Poker Hands Comparator/HandTypes/IHandType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poker_Hands_Comparator.HandTypes
+{
+    interface IHandType
+    {
+        public int Score { get; private set; }
+        public List<int> OrderedCardValues { get; private set; }
+        public string ToString();
+    }
+}
