497a5e7cbebe297524d5a6e7fa572875c7d9a355
Jacob Walker <jacob.walker94+github@gmail.com>
2013-04-06 18:56:21 +0100

    Added enum HandTypes to PokerHands namespace

---
+++ Poker Hands Comparator/HandTypes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokerHands
+{
+    enum HandTypes
+    {
+        Junk = 1,
+        Pair = 2,
+        TwoPairs = 3,
+        ThreeOfAKind = 4,
+        Straight = 5,
+        Flush = 6,
+        FullHouse = 7,
+        FourOfAKind = 8
+    }
+}
