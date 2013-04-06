8499f055320e29f315a7d39b375d8ca222106bb7
Jacob Walker <jacob.walker94+github@gmail.com>
2013-04-06 22:01:52 +0100

    Added very basic Scorer skeleton

---
+++ Poker Hands Comparator/Scorer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokerHands
+{
+    static class Scorer
+    {
+        public static HandType ScoreHand(Hand hand)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
