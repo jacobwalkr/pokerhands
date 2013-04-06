063ba872649db52a65372480aa2a709335ae4d92
Jacob Walker <jacob.walker94+github@gmail.com>
2013-04-06 22:58:11 +0100

    Implemented CheckStraightFlush in Scorer

--- Poker Hands Comparator/Scorer.cs
+++ Poker Hands Comparator/Scorer.cs
@@ -10,7 +10,34 @@ namespace PokerHands
     {
         public static HandType ScoreHand(Hand hand)
         {
-            throw new NotImplementedException();
+            // Cascade through score methods until a match is found
+            if (Scorer.CheckStraightFlush(hand))
+            {
+                return HandType.StraightFlush;
+            }
+        }
+
+        private static bool CheckStraightFlush(Hand hand)
+        {
+            int previousValue = hand[0].Value;
+            char previousSuit = hand[0].Suit;
+
+            for (int cardIndex = 1; cardIndex < 5; cardIndex++)
+            {
+                int currentValue = hand[cardIndex].Value;
+                char currentSuit = hand[cardIndex].Suit;
+
+                if (((currentValue - previousValue) != 1) || (previousSuit != currentSuit))
+                {
+                    // If the values aren't consecutive or the suits aren't the same
+                    return false;
+                }
+
+                previousValue = currentValue;
+                previousSuit = currentSuit;
+            }
+
+            return true;
         }
     }
 }
