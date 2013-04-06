9f618203b85da8abf12a274e62102aa5dc8e1c22
Jacob Walker <jacob.walker94+github@gmail.com>
2013-04-06 17:39:16 +0100

    Made value and suit getters public

--- Poker Hands Comparator/Card.cs
+++ Poker Hands Comparator/Card.cs
@@ -2,13 +2,13 @@
 {
     class Card
     {
-        private int value;
-        private char suit;
+        public int Value { get; private set; }
+        public char Suit { get; private set; }
 
         public Card(int _value, char _suit)
         {
-            this.value = _value;
-            this.suit = _suit;
+            this.Value = _value;
+            this.Suit = _suit;
         }
     }
 }
